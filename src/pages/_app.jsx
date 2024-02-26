ce09e3f19d291cd99e9e0f0f92961a2c6dc2c4b2
Usama Ali <75754522+Usama-26@users.noreply.github.com>
2024-02-26 13:03:43 +0500

    redux store replaced

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import { Provider } from "react-redux";
-import store from "./store";
+import store from "../store";
 
 const inter = Inter({
   subsets: ["latin"],
